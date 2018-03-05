00c848fd69efb90be6d43ab2250ff9a9ce50a943
Dev42013 <rickd1977@gmail.com>
2018-03-05 15:31:20 +0900

    Added follow path coroutine for enemy

--- Assets/EnemyMovement.cs
+++ Assets/EnemyMovement.cs
@@ -9,16 +9,20 @@ public class EnemyMovement : MonoBehaviour {
     [SerializeField] List<WayPoint> path;
 
 	// Use this for initialization
-	void Start () {
-        PrintAllWaypoints();
+	void Start () {     
+        StartCoroutine(FollowPath());
 	}
 
-    private void PrintAllWaypoints()
+    IEnumerator FollowPath()
     {
+        print("Starting Patrol");
         foreach (WayPoint wp in path)
         {
-            print(wp.name);
+            transform.position = wp.transform.position;
+            print("Visiting block: " + wp);
+            yield return new WaitForSeconds(1f);
         }
+        print("Ending Patrol");
     }
 
     // Update is called once per frame
