4bee51e71d8914f3e3a13a2090fdfb8a66f65c2e
Dev42013 <rickd1977@gmail.com>
2018-03-06 20:10:23 +0900

    Created simple enemy spawner
    Game will be continued from May because the lesson have stopped now.

---
+++ Assets/EnemySpawner.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour {
+
+    [SerializeField] float spawnDelay = 5f;
+    [SerializeField] EnemyMovement enemyPrefab;
+
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(SpawnEnemies());
+	}
+	
+	IEnumerator SpawnEnemies()
+    {
+        while (true) // forever
+        {
+            Instantiate(enemyPrefab, transform.position, Quaternion.identity);
+            yield return new WaitForSeconds(spawnDelay);
+        }    
+    }
+}
