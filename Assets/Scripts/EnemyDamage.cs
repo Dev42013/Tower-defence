0d7534d053ddb5a313e8c7a78279bd033047b23c
Dev42013 <rickd1977@gmail.com>
2018-03-06 15:22:58 +0900

    Built Towers that fire at enemies

---
+++ Assets/Scripts/EnemyDamage.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyDamage : MonoBehaviour {
+
+
+    [SerializeField] Collider collisionMesh;
+    [SerializeField] int hitPoints = 10;
+
+	void Start () {
+		
+	}
+
+    private void OnParticleCollision(GameObject other)
+    {
+        ProcessHit();
+        if (hitPoints <= 0)
+        {
+            KillEnemy();
+        }
+    }
+
+
+    void ProcessHit()
+    {
+        hitPoints -= 1;//todo change to variable    
+        print("Current hit points are " + hitPoints +"points");
+    }
+
+    private void KillEnemy()
+    {
+        Destroy(gameObject);
+    }
+
+}
--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -17,7 +17,7 @@ public class EnemyMovement : MonoBehaviour {
         foreach (WayPoint wp in path)
         {
             transform.position = wp.transform.position;
-            yield return new WaitForSeconds(1f);
+            yield return new WaitForSeconds(2f);
         }
         print("Ending Patrol");
     }
